84232aed647a360919b650602f23c26359a7f381
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-12 15:31:46 +0530

    Initial Commit

--- Patterns.java
+++ Patterns.java
@@ -1,99 +1,100 @@
 public class Patterns {
 
     public static void main(String[] args) {
-      //  Solid rectangle
-//        for(int i=1;i<=4;i++){
-//            for(int j=1;j<=5;j++) {
-//                System.out.print('*');
-//            }
-//            System.out.println();
-//        }
+      
+  // Solid rectangle
+  
+        for(int i=1;i<=4;i++){
+           for(int j=1;j<=5;j++) {
+               System.out.print('*');
+           }
+           System.out.println();
+       }
 
-        //Hollow Rectangle
-//        int n=4;
-//        int m=5;
-//          for(int i=1;i<=n;i++){
-//              for(int j=1;j<=m;j++){
-//                  if(i==1 || i==n || j==1 || j==m){
-//                      System.out.print('*');
-//                  }
-//                  else {
-//                      System.out.print(" ");
-//                  }
-//              }
-//              System.out.println();
-//          }
+ // Hollow Rectangle
+ 
+       int n=4;
+       int m=5;
+         for(int i=1;i<=n;i++){
+             for(int j=1;j<=m;j++){
+                 if(i==1 || i==n || j==1 || j==m){
+                     System.out.print('*');
+                 }
+                 else {
+                     System.out.print(" ");
+                 }
+             }
+             System.out.println();
+         }
 
+//  Half Pyramid
 
+       int n1=4;
+       for(int i=1;i<=n1;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
 
+// Reverse Half pyramid
 
-        // Half Pyramid
+       int n2=4;
+       for(int i=n2;i>=1;i--){
+           for(int j=1;j<=i;j++){
+               System.out.print("*");
+           }
+           System.out.println();
+       }
 
-//        int n1=4;
-//        for(int i=1;i<=n1;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+// Inverted half pyramid(180 degree)
+       int n=4;
+       for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+           System.out.println();
+       }
 
-       //  Reverse Half pyramid
+// Half Pyramid numbers
 
-//        int n2=4;
-//        for(int i=n2;i>=1;i--){
-//            for(int j=1;j<=i;j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+       int n1=4;
+       for(int i=1;i<=n1;i++){
+           for(int j=1;j<=i;j++){
+               System.out.print(j);
+           }
+           System.out.println();
+       }
 
-//// Inverted half pyramid(180 degree)
-//        int n=4;
-//        for(int i=1;i<=n;i++){
-//             for(int j=1;j<=n-i;j++) {
-//                 System.out.print(" ");
-//             }
-//             for(int j=1;j<=i;j++){
-//                 System.out.print("*");
-//             }
-//            System.out.println();
-//        }
+ // Inverted Half pyramid numbers
+       int n1=4;
+       for(int i=4;i>=1;i--){
+           for(int j=1;j<=i;j++){
+               System.out.print(j);
+           }
+           System.out.println();
+       }
 
-        // Half Pyramid numbers
+ //  Floyd's Triangle
+       int n1=4;
+       int count = 1;
+       for(int i=1;i<=n1;i++){
 
-//        int n1=4;
-//        for(int i=1;i<=n1;i++){
-//            for(int j=1;j<=i;j++){
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
+           for(int j=1;j<=i;j++){
 
-        // Inverted Half pyramid numbers
-//        int n1=4;
-//        for(int i=4;i>=1;i--){
-//            for(int j=1;j<=i;j++){
-//                System.out.print(j);
-//            }
-//            System.out.println();
-//        }
+               System.out.print(count);
+               count++;
 
-//Floyd's Triangle
-//        int n1=4;
-//        int count = 1;
-//        for(int i=1;i<=n1;i++){
-//
-//            for(int j=1;j<=i;j++){
-//
-//                System.out.print(count);
-//                count++;
-//
-//            }
-//            System.out.println();
-//
-//        }
+           }
+           System.out.println();
 
-        // 0-1 Triangle
+       }
+
+  // 0-1 Triangle
+       
         int n1=5;
         for(int i=1;i<=n1;i++){
             for(int j=1;j<=i;j++){
