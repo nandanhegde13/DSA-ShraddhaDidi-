bf962730ea2f32e4aa77a87336766d4434b52490
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-13 11:00:28 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -110,6 +110,121 @@ public class Patterns {
             System.out.println();
         }
         
+   // Inverted pyramid     
+         int n=4;
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=(n-i)+1;j++){
+               System.out.print(i);
+           }
+           System.out.println();
+       }
+        
+        
+        
+        //Butterfly pattern
+         int n = 4;
+ 
+       //upper part
+       for(int i=1; i<=n; i++) {
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+ 
+           int spaces = 2 * (n-i);
+           for(int j=1; j<=spaces; j++) {
+               System.out.print(" ");
+           }
+ 
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+ 
+ for(int i=n; i>=1; i--) {
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+ 
+           int spaces = 2 * (n-i);
+           for(int j=1; j<=spaces; j++) {
+               System.out.print(" ");
+           }
+ 
+           for(int j=1; j<=i; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+     
+        
+        // Rombous pattern
+        int n=5
+            for(int i=1; i<=n; i++) {
+           //spaces
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+ 
+           //stars
+           for(int j=1; j<=n; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+        
+        
+        
+        
+        //Palindromic pyramid
+          int n = 5;
+       for(int i=1; i<=n; i++) {
+           //spaces
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+ 
+           //first part
+           for(int j=i; j>=1; j--) {
+               System.out.print(j);
+           }
+ 
+           //second part
+           for(int j=2; j<=i; j++) {
+               System.out.print(j);
+           }
+           System.out.println();
+       }
+
+        
+        //Dimond pattern
+        int n = 5;
+ 
+       //upper part
+       for(int i=1; i<=n; i++) {
+           //spaces
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+           for(int j=1; j<=2*i-1; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+ 
+       //lower part
+       for(int i=n; i>=1; i--) {
+           //spaces
+           for(int j=1; j<=n-i; j++) {
+               System.out.print(" ");
+           }
+           for(int j=1; j<=2*i-1; j++) {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+
+
         
     }
 }
