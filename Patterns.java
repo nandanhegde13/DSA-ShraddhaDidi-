03f67225d54fd1ccd72ef17d86dba656025d227a
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-18 22:19:23 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -1,6 +1,23 @@
 public class Patterns {
 
     public static void main(String[] args) {
+        
+        // Z Pattern
+         int n = scan.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for(int j = n; j >= 1; j--) {
+
+                if (i == 1 || i == n || i == j) {
+                    System.out.print("# ");
+                }
+
+                else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
       
   // Solid rectangle
   
