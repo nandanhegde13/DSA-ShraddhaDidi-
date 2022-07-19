32252c865ddb05d18ec0d7be3090374e8e0a915d
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-19 12:45:19 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -2,6 +2,24 @@ public class Patterns {
 
     public static void main(String[] args) {
         
+        // W pattern
+      Scanner scan = new Scanner(System.in);
+      int n = scan.nextInt();
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=n;j++){
+              if(j==1 || j==n){
+                  System.out.print("*\t");
+              }
+              else if ((i>n/2) && (i==j || i+j == n+1 )){
+                  System.out.print("*\t");
+              }
+              else{
+                  System.out.print("\t");
+              }
+          }
+          System.out.println();
+      }
+        
         // Z Pattern
          int n = scan.nextInt();
 
