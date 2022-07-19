45efd7600b86be2e875dc76a9159ca254bcfb848
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-19 06:28:16 +0530

    Create benjaminBulb.java

---
+++ benjaminBulb.java
@@ -0,0 +1,8 @@
+    public static void benjaminBulb(){
+        Scanner scan = new Scanner(System.in);
+        int n= scan.nextInt();
+        for(int i=0;i*i<=n;i++){
+            System.out.println(i*i);
+        }
+
+    }
