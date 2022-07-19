3996f64e934d1e1e9bca13534891c80f3789dc8f
Nandan Hegde <55938000+nandanhegde13@users.noreply.github.com>
2022-07-19 06:30:11 +0530

    Create pythagorianTriplet.java

---
+++ pythagorianTriplet.java
@@ -0,0 +1,28 @@
+public static void pythagorianTriplet(){
+   Scanner scan = new Scanner(System.in);
+   int a = scan.nextInt();
+   int b=scan.nextInt();
+   int c= scan.nextInt();
+   int max = a;
+
+   if(b>=max){
+       max=b;
+   }
+   if(c>max){
+       max=c;
+   }
+
+   if(max==a){
+       boolean flag = (b*b + c*c) == (a*a);
+       System.out.println(flag);
+   }
+   else if(max==b){
+       boolean flag = (a*a + c*c) == (b*b);
+        System.out.println(flag);
+   }
+   else{
+       boolean flag = (a*a + b*b) == (c*c);
+       System.out.println(flag);
+   }
+
+   }
